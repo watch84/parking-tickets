5b654bd92cf04e387d835c9726c9f5c6661dd232
IHIutch <jbhutch01@gmail.com>
2018-04-15 15:12:18 -0400

    header/footer

---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>parking tickets</title>
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" href="css/main.css">
+    
+	<link href="https://fonts.googleapis.com/css?family=Days+One" rel="stylesheet">
+	
+  </head>
+  <body>

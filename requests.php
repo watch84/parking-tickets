3983b1a8d3617a2ee2bc1659bb3b44946da23d4c
IHIutch <jbhutch01@gmail.com>
2018-04-19 00:17:02 -0400

    Map fixes

--- requests.php
+++ requests.php
@@ -1,72 +1,65 @@
 <?php include "inc/header.php"?>
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-12 max-height px-0">
-			<div id="map"></div>
-			<div class='map-overlay'>
-				<div id='markers'></div>
-			</div>
+    <div class="row">
+        <div class="col-12 max-height px-0">
+            <div id="map"></div>
+            <div class='map-overlay'>
+                <div id='markers'></div>
+            </div>
 
-<script>
-    mapboxgl.accessToken = 'pk.eyJ1IjoiamJodXRjaCIsImEiOiJjamRqZGU1eTYxMTZlMzNvMjV2dGxzdG8wIn0.IAAk5wKeLXOUaQ4QYF3sEA';
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v10',
-        center: [-78.87, 42.91],
-        zoom: 12.
-    });
+            <script>
+                mapboxgl.accessToken = 'pk.eyJ1IjoiamJodXRjaCIsImEiOiJjamRqZGU1eTYxMTZlMzNvMjV2dGxzdG8wIn0.IAAk5wKeLXOUaQ4QYF3sEA';
+                var map = new mapboxgl.Map({
+                    container: 'map',
+                    style: 'mapbox://styles/mapbox/streets-v10',
+                    center: [-78.87, 42.91],
+                    zoom: 12.
+                });
 
-    
-    var route = document.getElementById('route');
-    var stops = document.getElementById('stops');
-    
-    map.on('load', function() {
+                map.on('load', function() {
 
-        map.addLayer(
-            <?php searches();?>
-        );
+                    map.addLayer(
+                        <?php searches();?>
+                    );
 
-        var popup = new mapboxgl.Popup({
-            closeButton: false,
-            closeOnClick: false
-        });
-        
+                    var popup = new mapboxgl.Popup({
+                        closeButton: false,
+                        closeOnClick: false
+                    });
 
-        map.on('mouseenter', 'route', function(e) {
-            // Change the cursor style as a UI indicator.
-            map.getCanvas().style.cursor = 'pointer';
-            map.setPaintProperty('route', 'line-width', 8);
-            
-            // Render found features in an overlay.
-            route.innerHTML = '';
-            
-            var id = document.createElement('div');
-            id.textContent = 'Route ID: ' + e.features[0].properties.route_id;
-            route.appendChild(id);
-            route.style.display = 'block';
-        });
-        
-        map.on('mouseenter', 'points', function(e) {
-            points.innerHTML = '';
-            
-            var name = document.createElement('div');
-            name.textContent = 'Stop Name: ' + e.features[0].properties.name;
-            points.appendChild(name);
-            points.style.display = 'block';
-        });
 
-        map.on('mouseleave', 'route', function() {
-            map.getCanvas().style.cursor = '';
-            map.setPaintProperty('route', 'line-width', 4);
-            route.style.display = 'none';
-        });
-        
-        map.on('mouseleave', 'points', function() {
-            points.style.display = 'none';
-        });
-    });
+                    // When a click event occurs on a feature in the places layer, open a popup at the
+                    // location of the feature, with description HTML from its properties.
+                    map.on('click', 'points', function(e) {
+                        var coordinates = e.features[0].geometry.coordinates.slice();
+                        var num = e.features[0].properties.add_num;
+                        var streetname = e.features[0].properties.add_street;
 
-</script>
-		</div>
-	</div>
+                        // Ensure that if the map is zoomed out such that multiple
+                        // copies of the feature are visible, the popup appears
+                        // over the copy being pointed to.
+                        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                        }
+
+                        new mapboxgl.Popup()
+                            .setLngLat(coordinates)
+                            .setHTML(num + " " + streetname)
+                            .addTo(map);
+                    });
+
+                    // Change the cursor to a pointer when the mouse is over the places layer.
+                    map.on('mouseenter', 'points', function() {
+                        map.getCanvas().style.cursor = 'pointer';
+                    });
+
+                    // Change it back to a pointer when it leaves.
+                    map.on('mouseleave', 'points', function() {
+                        map.getCanvas().style.cursor = '';
+                    });
+                });
+
+            </script>
+        </div>
+    </div>
 </div>
